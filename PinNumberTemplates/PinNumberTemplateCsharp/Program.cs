d4f86fe4667e7b8e029d8ab30b9b671024bb5709
Mikołaj Strusiński <mikistrus@gmail.com>
2020-02-29 10:30:54 +0100

    Main Programme

---
+++ PinNumberTemplates/PinNumberTemplateCsharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PinNumberTemplateCsharp
+{
+    class Program
+    {
+        const string PIN = "1234";
+
+        static void Main(string[] args)
+        {
+            Pin();
+        }
+
+        static void Pin()
+        {
+            int counter = 3;
+            for (int i = 0; i < 3; i++)
+            {
+                counter--;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Enter the PIN number: ");
+                string user_input = Console.ReadLine();
+                if (user_input==PIN)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Correct Pin!");
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Incorrect Pin, try again.");
+                    Console.Write($"Attempts left: {counter}");
+                    Console.WriteLine("");
+                }
+
+            }
+        }
+    
+    }
+}
